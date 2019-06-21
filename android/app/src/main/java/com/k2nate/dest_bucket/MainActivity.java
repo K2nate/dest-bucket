ba1cc85c118c7b4d1af5167c8860e10aa5e9456e
K2nate <kohanawa.k@gmail.com>
2019-06-21 19:06:48 +0800

    first commit

---
+++ android/app/src/main/java/com/k2nate/dest_bucket/MainActivity.java
@@ -0,0 +1,13 @@
+package com.k2nate.dest_bucket;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
